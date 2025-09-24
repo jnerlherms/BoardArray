4c7c025f862e742e595edb76169eb85dc4a97d0b
Jon Erlo Hermosa <jnerlherms@gmail.com>
2025-09-24 12:09:16 +0800

    Update boardarray.h

--- src/boardarray.h
+++ src/boardarray.h
@@ -18,23 +18,45 @@ class BoardArray : public Board {
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
 
             int i, j;
-            for(i = 0; i < SIZE; i++){
-                if(entry->compare(&array[i])){
-                    for(j = SIZE - 1; j > i; j--){
+            for(i = 0; i < index; i++){
+              Entry existing = array[i]; // *(array + i);
+                if(entry->compare(&existing)){
+                    for(j = index; j > i; j--){
+                      if(index == SIZE){
+                        continue;
+                      }
                         array[j] = array[j - 1];
                     }
                   array[i] = *entry;
-                  index++;
+                  if(index < SIZE){
+                    index++;
+                  }
                   return;
                 }
-                // else{
-                //   cout << entry->name << "'s score is too low to be added!" << endl;
-                //   return;
-                // }
             }
 
-            cout << entry->name << "'s score is too low to be added!" << endl;
+            if(index < SIZE){
+              arr[index] = *entry;
+              index++;
+            }
+            else {
+              cout << entry->name << "'s score is too low to be added!" << endl;
+            }
 
+            // int i, j;
+            // for(i = 0; i < SIZE; i++){
+            //     if(entry->compare(&array[i])){
+            //         for(j = SIZE - 1; j > i; j--){
+            //             array[j] = array[j - 1];
+            //         }
+            //       array[i] = *entry;
+            //       index++;
+            //       return;
+            //     }
+            // }
+            //
+            // cout << entry->name << "'s score is too low to be added!" << endl;
+            return;
         }
 
         void print() {
